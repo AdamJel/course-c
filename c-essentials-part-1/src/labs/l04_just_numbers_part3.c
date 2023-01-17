c863a03ebe40436452feac7df6dd6b1a93e644a0
Adam Jelinek <ajelinek@redhat.com>
2023-01-16 18:52:04 -0700

    add: lab 04: just numbers part3

---
+++ c-essentials-part-1/src/labs/l04_just_numbers_part3.c
@@ -0,0 +1,19 @@
+/*
+
+#include <stdio.h>
+
+int main()
+{
+    printf("The value of seven is: %d \n", 0087);
+    printf("The value of eight is: %d \n", (8000x - 7992x)/x);
+}
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("The value of seven is: %d \n", 7);
+    printf("The value of eight is: %d \n", (8000 - 7992) / 1);
+    return 0;
+}
